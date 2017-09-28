3bbd4c6985e26e74d3e743ed895e0e32e25623b3
Jason Navarro <Jasonnavarro86@yahoo.com>
2017-09-27 20:25:24 -0400

    Added Create card functions now need to figure out how to show them.

--- BasicTrivia.js
+++ BasicTrivia.js
@@ -2,73 +2,76 @@ var inquirer = require("inquirer")
 var flashCard = require('./library/BasicCardGenorator.js')
 var BasicJSON = require('./Basic');
 // I create my new constructors 
-var constructorArray = [{
-        question: flashCard("What is Bob Marley's real name?", 'Robert Nesta Marley')
-    },
-    {
-        question: flashCard('Where was Bob Marley born?', 'Nine Mile Jamaica')
-    },
-    {
-        question: flashCard('What was the name of the group where Marley started his career?', 'The Wailers')
-    },
-    {
-        question: flashCard('Bob was bullied and derogatorily nicknamed what by his neighbors growing up?', 'White Boy')
-    },
-    {
-        question: flashCard("What was Marley's first hit outside Jamaica?", 'No Woman No Cry')
-    }
-]
-// Welcome logs
-console.log("\nWelcome to Bob Marley Trivia");
-console.log("________________________________\n");
 // Score Keeper and loop Variable 
 var score = 0;
 var loopInt = 0;
-// This function prompts Do You Want To Play? 
+var jsonNum = 0;
+
+// Welcome logs
+console.log("\n Welcome To Flash N Cloze\n");
+
+// This function starts the game and asks the user what they want to do. 
 var startGame = function () {
+
     inquirer
         .prompt([{
-            type: 'confirm',
-            message: "Want To Play?\n",
-            name: 'confirm',
-            default: true
+            type: "list",
+            message: "What Would You Like To Do?",
+            choices: ["Play Bob Marley Trivia", "Play Movie Trivia", "Play NBA Nick Name Trivia", "Build My Own Flash Card", "Build My Own Cloze Card", "Im Done"],
+            name: "answer"
         }, ])
         .then(function (inquirerResult) {
-            if (inquirerResult.confirm == false) {
-                console.log("\nSorry To Hear, Come Back When You Want To Play!\n");
-            } else {
-                generateQuestions();
+
+            switch (inquirerResult.answer) {
+                case "Play Bob Marley Trivia":
+                    TriviaQuestions()
+                    break;
+                case "Play Movie Trivia":
+                    jsonNum = 1;
+                    TriviaQuestions()
+                    break;
+                case "Play NBA Nick Name Trivia":
+                    jsonNum = 2;
+                    TriviaQuestions()
+                    break;
+                case "Build My Own Flash Card":
+                    
+                    break;
+                case "Build My Own Cloze Card":
+                    
+                    break;
+                default:
+                    console.log('Have A Great Day!');
             }
         });
 }
 startGame();
+
 // Main Game Generator 
-var generateQuestions = function () {
-    if (loopInt < 5) {
+var TriviaQuestions = function () {
+    // I create my new constructors 
+
+    if (loopInt < BasicJSON[jsonNum].Questions.length) {
+
+        var constructorArray = {
+            question: flashCard(BasicJSON[jsonNum].Questions[loopInt], BasicJSON[jsonNum].Answers[loopInt])
+        }
+
         inquirer
             .prompt([{
-                    message: constructorArray[loopInt].question.front,
-                    name: 'answer',
-                },
-            ])
+                message: constructorArray.question.front,
+                name: 'answer',
+            }, ])
             .then(function (inquirerResult) {
-                if (inquirerResult.answer == constructorArray[loopInt].question.back.toLowerCase()) {
+
+                if (inquirerResult.answer == constructorArray.question.back.toLowerCase()) {
                     score++;
                 }
                 loopInt++;
-                generateQuestions();
+                TriviaQuestions();
             })
     } else {
-        console.log("\nThank You For Playing Bob Marley Trivia! You answered " + score + " correct out of 5.\n");
+        console.log("\nThank You For Playing " + BasicJSON[jsonNum].Category + "! You answered " + score + " correct out of 5.\n");
+        startGame();
     }
 }
-var server = http.createServer(function(req, res){
-
-    res.writeHead(200, {'Content-Type':'text/html'});
-
-    var myStream = fs.createReadStream(__dirname + '/flash.html', 'utf8');
-
-    myStream.pipe(res);
-    // res.end("\nYou answered " + score + " correct out of 5.\n")
-})
- server.listen(2222);
--- ClozeTrivia.js
+++ ClozeTrivia.js
@@ -1,12 +1,15 @@
 var inquirer = require("inquirer")
 var ClozeCard = require('./library/ClozeCardGenorator.js')
 var BasicCard = require('./library/BasicCardGenorator.js')
-var JSONS = require('./cloze')
+var ClozeJson = require('./cloze')
 
-// Score Keeper and loop Variable 
+// Score Keeper ,loop Variables, and custom card arrays.
 var score = 0;
 var loopInt = 0;
 var jsonNum = 0;
+var clozeNum = 0;
+var createFullText = [];
+var createClozeText = [];
 
 // Welcome logs
 console.log("\n Welcome To Flash N Cloze\n");
@@ -15,56 +18,123 @@ console.log("\n Welcome To Flash N Cloze\n");
 var startGame = function () {
 
     inquirer
-        .prompt([  {
+        .prompt([{
             type: "list",
             message: "What Would You Like To Do?",
-            choices: ["Play Bob Marley Trivia", "Play Movie Trivia", "Play NBA Nick Name Trivia", "Build My Own Flash Card", "Build My Own Cloze Card"],
+            choices: ["Play Bob Marley Trivia", "Play Movie Trivia", "Play NBA Nick Name Trivia", "Build My Own Cloze Card", "Show My Made Cards", "Im Done"],
             name: "answer"
-          }, ])
+        }, ])
         .then(function (inquirerResult) {
-            if (inquirerResult.answer == "Play Bob Marley Trivia") {
-                TriviaQuestions()
-            }else if(inquirerResult.answer == "Play Movie Trivia"){ 
-                jsonNum = 1;
-                TriviaQuestions()
-            }else if(inquirerResult.answer == "Play NBA Nick Name Trivia"){ 
-                jsonNum = 2;
-                TriviaQuestions()
-            }else if(inquirerResult.answer == "Build My Own Flash Card"){ 
-                
-            }else if(inquirerResult.answer == "Build My Own Cloze Card"){ 
-                
-            }else {
-                // generateQuestions();
+
+            switch (inquirerResult.answer) {
+                case "Play Bob Marley Trivia":
+                    TriviaQuestions()
+                    break;
+                case "Play Movie Trivia":
+                    jsonNum = 1;
+                    TriviaQuestions()
+                    break;
+                case "Play NBA Nick Name Trivia":
+                    jsonNum = 2;
+                    TriviaQuestions()
+                    break;
+                case "Build My Own Cloze Card":
+                    createClozeFull()
+                    break;
+                case "Show My Made Cards":
+
+                    break;
+                default:
+                    console.log('Have A Great Day!');
             }
         });
 }
 startGame();
 
-
 // Main Game Generator 
 var TriviaQuestions = function () {
-    // I create my new constructors 
-var constructorArray = {
-    question: ClozeCard(JSONS[jsonNum].FullText[itemNum], JSONS[jsonNum].Cloze[itemNum])
-}
-    if (loopInt < 5) {
+
+
+    if (loopInt < ClozeJson[jsonNum].FullText.length) {
+
+        // I CREATE NEW INSTANCES OF CLOZE CARDS DYNAMICALLY 
+        var constructorArray = {
+            question: ClozeCard(ClozeJson[jsonNum].FullText[loopInt], ClozeJson[jsonNum].Cloze[loopInt])
+        }
 
         inquirer
             .prompt([{
-                    message: constructorArray.question.partial,
-                    name: 'answer',
-                },
-            ])
+                message: constructorArray.question.partial,
+                name: 'answer',
+            }, ])
             .then(function (inquirerResult) {
 
                 if (inquirerResult.answer == constructorArray.question.cloze.toLowerCase()) {
-                    score++;
+                    score++
                 }
-                loopInt++;
-                TriviaQuestions();
+                loopInt++
+                TriviaQuestions()
             })
     } else {
-        console.log("\nThank You For Playing"+ +"! You answered " + score + " correct out of 5.\n");
+        console.log("\nThank You For Playing " + ClozeJson[jsonNum].Category + "! You answered " + score + " correct out of 5.\n");
+        score = 0;
+        loopInt = 0;
+        startGame()
     }
 }
+
+
+function createClozeFull() {
+    if (clozeNum < 5) {
+        inquirer
+            .prompt([{
+                message: ClozeJson[3].FullText[clozeNum],
+                name: 'answer',
+                validate: function (input) {
+                    if (input === '') {
+                        console.log('Please provide Full Text');
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }
+            }, ])
+            .then(function (inquirerResult) {
+
+                createFullText.push(inquirerResult.answer)
+
+                createClozePartial();
+            })
+    } else {
+        loopInt = 0;
+        clozeNum = 0;
+        console.log('Card Made');
+        startGame();
+    }
+}
+
+function createClozePartial() {
+
+    inquirer
+        .prompt([{
+            message: ClozeJson[3].Cloze[clozeNum],
+            name: 'answer',
+            validate: function (input) {
+                if (input === '' || input != createFullText[clozeNum]) {
+                    console.log('Please Make Sure Your Cloze is part of the Full Text');
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }])
+        .then(function (inquirerResult) {
+
+            createClozeText.push(inquirerResult.answer)
+            loopInt++
+            clozeNum++;
+            createClozeFull();
+        })
+
+}
+
